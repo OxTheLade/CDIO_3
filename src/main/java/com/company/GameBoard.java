1d56b496fa8e9d57b335db0c93455d14d928dd9a
Jawahir farah <tbs7bjawahir@gmail.com>
2020-11-20 14:01:40 +0100

    Halvdelen af spillepladen lavet

--- src/main/java/com/company/GameBoard.java
+++ src/main/java/com/company/GameBoard.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import gui_codebehind.GUI_Center;
 import gui_fields.*;
 import gui_main.GUI;
 
@@ -9,17 +10,30 @@ public class GameBoard {
 
     public GUI gui;
     public int maxPlayers;
-    public GUI_Field[] fields;
+    public GUI_Field[] fields = new GUI_Field[]{
+            new GUI_Start(),
+            new GUI_Street(),
+            new GUI_Street(),
+            new GUI_Chance(),
+            new GUI_Street(),
+            new GUI_Street(),
+            new GUI_Jail(),
+            new GUI_Street(),
+            new GUI_Street(),
+            new GUI_Chance(),
+            new GUI_Street(),
+            new GUI_Street(),
+            new GUI_Tax(),
+            new GUI_Street(),
+            new GUI_Street(),
+
+
+
+    };
 
     public void createBoard() {
 
 
-        fields = new GUI_Field[]{
-                new GUI_Start(),
-                new GUI_Street(),
-                new GUI_Street(),
-                new GUI_Chance()
-        };
         fields[0].setTitle("Start");
 
         fields[1].setTitle("Burgerbaren");
@@ -27,6 +41,28 @@ public class GameBoard {
         fields[2].setTitle("Pizzaria");
 
         fields[3].setTitle("Chance");
+
+        fields[4].setTitle("Slikbutikken");
+
+        fields[5].setTitle("Iskiosken");
+
+        fields[6].setTitle("Fængsel");
+
+        fields[7].setTitle("Museum");
+
+        fields[8].setTitle("Biblotek");
+
+        fields[9].setTitle("Chance");
+
+        fields[10].setTitle("Skaterparken");
+
+        fields[11].setTitle("Svømmingpool");
+
+        fields[12].setTitle("Parkering");
+
+        fields[13].setTitle("Spillehallen");
+
+        fields[14].setTitle("Biografen");
 
 //        fields[2].setTitle("Pizzeriaet");
 //
@@ -45,6 +81,46 @@ public class GameBoard {
         GUI_Field Pizzaria = gui.getFields()[2];
         GUI_Ownable pizzariaOwnable = (GUI_Ownable) Pizzaria;
         pizzariaOwnable.setSubText("Pris: 2000");
+
+        GUI_Field Slikbutikken = gui.getFields()[4];
+        GUI_Ownable SlikbutikkenOwnable = (GUI_Ownable) Slikbutikken;
+        SlikbutikkenOwnable.setSubText("Pris 1500");
+
+        GUI_Field Iskiosken = gui.getFields()[5];
+        GUI_Ownable IskioskenOwnable = (GUI_Ownable) Iskiosken;
+        IskioskenOwnable.setSubText("Pris 2500");
+
+        GUI_Field Museum = gui.getFields()[5];
+        GUI_Ownable MuseumOwnable = (GUI_Ownable) Museum;
+        MuseumOwnable.setSubText("Pris 2500");
+
+        GUI_Field Biblotek = gui.getFields()[8];
+        GUI_Ownable BiblotekOwnable = (GUI_Ownable) Biblotek;
+        MuseumOwnable.setSubText("Pris 3000");
+
+        GUI_Field Skaterparken = gui.getFields()[10];
+        GUI_Ownable SkaterparkenOwnable = (GUI_Ownable) Skaterparken;
+        SkaterparkenOwnable.setSubText("Pris 2400");
+
+        GUI_Field Svømmingpool = gui.getFields()[11];
+        GUI_Ownable SvømmingpoolOwnable = (GUI_Ownable) Svømmingpool;
+        SvømmingpoolOwnable.setSubText("Pris 1400");
+
+        GUI_Field Spillehallen = gui.getFields()[13];
+        GUI_Ownable SpillehallenOwnable = (GUI_Ownable) Spillehallen;
+        SpillehallenOwnable.setSubText("Pris 2000");
+
+        GUI_Field Biografen = gui.getFields()[14];
+        GUI_Ownable BiografenOwnable = (GUI_Ownable) Biografen;
+        BiografenOwnable.setSubText("Pris 2700");
+
+
+
+
+
+
+
+
     }
 
 
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
         board.addProperties();
 
         /* Tilføjer spillere alt efter hvor mange spillere man er vha. et for loop og sætter alle spillerne i en array.
-           Hvis "i" tæller fra 0 og til op til fx max 2 spillere, så vil den forsøge at oprette 3 gendstande til array
+    y76ttr       Hvis "i" tæller fra 0 og til op til fx max 2 spillere, så vil den forsøge at oprette 3 gendstande til array
            Derfor starter vi at tælle fra 1 og bagefter trækker 1 fra, da array starter med index 0
            Her bliver spillerne gemt i "players" array fra Player klassen */
 
